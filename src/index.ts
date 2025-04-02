53e2a8dc11801f3798fef11d1c6717f8aa607870
byoussfi <byoussfi@steamulo.com>
2025-04-02 18:16:59 +0200

    feat: published lol

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,28 @@
 import * as path from "path";
 import * as fs from "fs-extra";
 
-// Get the correct source directory path whether running from source or compiled code
+/**
+ * Get the correct source directory path whether running from source or compiled code
+ * @returns The path to the rules directory
+ */
 function getRulesDir(): string {
-  return __dirname; // This will be either 'src' or 'dist/src'
+  return __dirname;
 }
 
+/**
+ * Get a rule by name
+ * @param ruleName - The name of the rule to get
+ * @returns The rule as a string
+ */
 export const getRule = (ruleName: string): string => {
   const rulePath = path.join(getRulesDir(), `${ruleName}.mdc`);
   return fs.readFileSync(rulePath, "utf-8");
 };
 
+/**
+ * List all available rules
+ * @returns An array of rule names
+ */
 export const listRules = (): string[] => {
   const rulesDir = getRulesDir();
   return fs
